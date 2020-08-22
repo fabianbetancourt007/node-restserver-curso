3a87c5a0df6e967912b97043471e383c41fd608c
fabian <fbeta90@gmail.com>
2020-08-22 13:01:26 +0200

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 
 } else {
-    urlDB = 'mongodb+srv://fabi:VQWkPXYmsKKgEOG8@cluster0.yd5qb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
